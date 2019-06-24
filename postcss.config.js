23f74710c2df1c2385d1393d5ebe5bb6206f4b6d
James Long <longster@gmail.com>
2019-06-24 16:09:35 -0400

    Disable CSS purging

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   plugins: [
     require('tailwindcss'),
-    require('autoprefixer'),
-    ...(process.env.NODE_ENV === 'production'
-      ? [
-          require('@fullhuman/postcss-purgecss')({
-            content: ['./templates/**/*'],
-            defaultExtractor: content =>
-              content.match(/[A-Za-z0-9-_:/]+/g) || []
-          })
-        ]
-      : [])
+    require('autoprefixer')
+    // ...(process.env.NODE_ENV === 'production'
+    //   ? [
+    //       require('@fullhuman/postcss-purgecss')({
+    //         content: ['./templates/**/*'],
+    //         defaultExtractor: content =>
+    //           content.match(/[A-Za-z0-9-_:/]+/g) || []
+    //       })
+    //     ]
+    //   : [])
   ]
 };
